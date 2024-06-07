cb3ce6631ddc1f8c42efe028b20efdf01da8abf8
JDorangetree <julian.naranjo2014@gmail.com>
2024-06-07 18:32:41 +0000

    first commit

---
+++ src/main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+from . import smokeTest
+
+#import debugpy
+
+#debugpy.listen(("0.0.0.0", 5678))
+# debugpy.wait_for_client()
+
+app = FastAPI()
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+app.include_router(smokeTest.router, prefix="/smoke-test")
+
+# Define the API endpoints
+@app.get('/')
+def health():
+    return {
+        "message": "OK 🚀"
+    }
+
+
+@app.post("/file/upload")
+def upload_file(file: UploadFile):
+    return {"filename": file.filename}
